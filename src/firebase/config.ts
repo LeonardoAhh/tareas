c7866d547004113df18ee3afbf2e7bfcc6ad1da2
Leonardo <capacitacionqrovino@gmail.com>
2026-01-08 18:21:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5066031299-cff98",
+  "appId": "1:226566165354:web:19ffd5a810055f14d54b13",
+  "apiKey": "AIzaSyAgDWEAdqwL4_BdFPIyr_CrN_HnWBtc5DA",
+  "authDomain": "studio-5066031299-cff98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "226566165354"
+};
